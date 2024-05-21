4200af5d32fe2ac8fd990077fffb27189637a41a
HopKaa <screams699@gmail.com>
2024-05-21 17:29:59 +0700

    end of game implemented

--- Assets/Scripts/Food/Food.cs
+++ Assets/Scripts/Food/Food.cs
@@ -1,22 +1,44 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Food : MonoBehaviour
 {
     [SerializeField] private BoxCollider playArea;
+    private SnakeMovement snakeMovement;
 
     private void Start()
     {
+        snakeMovement = FindObjectOfType<SnakeMovement>();
         RandomizePosition();
     }
 
     private void RandomizePosition()
     {
         Bounds bounds = playArea.bounds;
+        Vector3 newPosition;
+        bool isPositionValid;
 
-        float x = Random.Range(bounds.min.x, bounds.max.x);
-        float z = Random.Range(bounds.min.z, bounds.max.z);
+        do
+        {
+            float x = Random.Range(bounds.min.x, bounds.max.x);
+            float z = Random.Range(bounds.min.z, bounds.max.z);
+            newPosition = new Vector3(Mathf.Round(x), 0.5f, Mathf.Round(z));
 
-        transform.position = new Vector3(Mathf.Round(x), 0.5f, Mathf.Round(z));
+            isPositionValid = true;
+            List<Vector3> segmentPositions = snakeMovement.GetSegmentPositions();
+
+            foreach (Vector3 position in segmentPositions)
+            {
+                if (Vector3.Distance(newPosition, position) < 0.5f)
+                {
+                    isPositionValid = false;
+                    break;
+                }
+            }
+
+        } while (!isPositionValid);
+
+        transform.position = newPosition;
     }
 
     private void OnTriggerEnter(Collider other)
--- Assets/Scripts/Snake/SnakeMovement.cs
+++ Assets/Scripts/Snake/SnakeMovement.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 public class SnakeMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    [SerializeField] private float rotationSpeed = 200f;
     [SerializeField] private Transform segmentPrefab;
     [SerializeField] private float segmentSpacing = 1f; // Расстояние между сегментами
     private List<Transform> segments;
     private List<Vector3> positions;
+    private Vector3 direction = Vector3.forward;
+    private Vector3 previousDirection;
 
     private void Start()
     {
@@ -20,22 +22,47 @@ public class SnakeMovement : MonoBehaviour
 
     private void Update()
     {
+        HandleInput();
         MoveSnake();
         UpdateSegments();
+    }
+
+    private void HandleInput()
+    {
+        if (direction == Vector3.forward || direction == Vector3.back)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                direction = Vector3.left;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                direction = Vector3.right;
+            }
+        }
+        else if (direction == Vector3.left || direction == Vector3.right)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                direction = Vector3.forward;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                direction = Vector3.back;
+            }
+        }
+
+        previousDirection = direction;
     }
 
     private void MoveSnake()
     {
         float move = speed * Time.deltaTime;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
-
-        transform.Translate(Vector3.forward * move);
-        transform.Rotate(Vector3.up * rotation);
-
+        transform.Translate(direction * move);
         positions.Insert(0, transform.position);
 
         // Ограничиваем размер списка позиций
-        if (positions.Count > segments.Count + 1)
+        if (positions.Count > segments.Count)
         {
             positions.RemoveAt(positions.Count - 1);
         }
@@ -57,5 +84,33 @@ public class SnakeMovement : MonoBehaviour
         segment.position = newPosition;
         segments.Add(segment);
         positions.Add(segment.position);
+
+        // Запуск корутины для добавления коллайдера с задержкой
+        StartCoroutine(AddColliderWithDelay(segment));
+    }
+
+    private IEnumerator AddColliderWithDelay(Transform segment)
+    {
+        BoxCollider collider = segment.gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = true;
+        collider.enabled = false; // Отключаем коллайдер на начальном этапе
+        yield return new WaitForSeconds(1f); // Задержка в 1 секунду
+        segment.tag = "SnakeSegment"; // Присваиваем тег "SnakeSegment" сегменту после задержки
+        collider.enabled = true; // Включаем коллайдер после задержки
+    }
+
+    public List<Vector3> GetSegmentPositions()
+    {
+        return new List<Vector3>(positions);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SnakeSegment"))
+        {
+            // Столкновение головы змеи с сегментом - игрок проиграл
+            Debug.Log("Game Over! You collided with a snake segment!");
+            // Здесь можно добавить логику для завершения игры или перезапуска уровня
+        }
     }
 }
